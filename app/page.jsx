765d105921926774a114f22e89f730b914ceab2b
deepraj <deepraj.bharadava@codezeros.com>
2025-08-26 18:53:37 +0530

    nextjs

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,6 @@ const page = () => {
   return (
     <>
       <main className="flex min-h-screen flex-col p-6">
-        <div className={styles.shape} />
         <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
           <AcmeLogo />
         </div>
@@ -22,7 +21,7 @@ const page = () => {
               , brought to you by Vercel.
             </p>
             <Link
-              href=""
+              href="login?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fdashboard"
               className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
             >
               <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
